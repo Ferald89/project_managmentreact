17afccd8e385d3d062ec564ce3ea3ea50b3236a1
Fernando Aldair <ferpool_ald@hotmail.com>
2020-07-01 17:33:27 -0500

    home

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import logo from '../images/logod.svg'
+import './styles/Navbar.css'
 
 class Navbar extends React.Component{
     render(){
@@ -8,7 +9,7 @@ class Navbar extends React.Component{
             <div className="Navbar" >
                 <div className="container-fluid">
                     <Link>
-                        <img className="Navbar__bramd-logo" src="" alt="Logo"/>
+                        <img className="Navbar__brand-logo" src={logo} alt="Logo"/>
                         <span className="font-weight-light">Deica</span>
                         <span className="font-weight-bold">Project Managment</span>
                     </Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,41 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 // Local
-import Navbar from '../components/Navbar'
+import './styles/Home.css';
+import platziconfLogoImage from '../images/logod.svg';
+import astronautsImage from '../images/hero-image.svg';
 
 class Home extends React.Component{
     render(){
         return(
-            <h1>
-                Hello
-            </h1>
+            <div className="Home">
+            <div className="container">
+              <div className="row">
+                <div className="Home__col col-12 col-md-4">
+                  <img
+                    src={platziconfLogoImage}
+                    alt="Platzi Conf Logo"
+                    className="img-fluid mb-2"
+                  />
+    
+                  <h1>Sistema De Control De Projetos</h1>
+                  <Link className="btn btn-primary" to="/badges">
+                    Start
+                  </Link>
+                </div>
+    
+                <div className="Home__col d-none d-md-block col-md-8">
+                  <img
+                    src={astronautsImage}
+                    alt="Astronauts"
+                    className="img-fluid p-10"
+                    height="50%"
+                    width="50%"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
         )
     }
 }
